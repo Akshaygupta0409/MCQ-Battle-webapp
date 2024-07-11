83b0c653e0ff56fe899f0949469663c8289556e2
akshay kumar gupta <guptaakshay0409@gmail.com>
2024-07-11 12:18:13 +0530

    Naming Convention Error Resolved

--- Frontend/mcq_frontend/src/App.jsx
+++ Frontend/mcq_frontend/src/App.jsx
@@ -6,7 +6,7 @@ import AppRouter from "./router/AppRouter";
 function App() {
   return (
     <div className="">
-      new page
+     
       <MainLayout>
         <AppRouter />
       </MainLayout>
--- Frontend/mcq_frontend/src/services/api/auth/auth.service.js
+++ Frontend/mcq_frontend/src/services/api/auth/auth.service.js
@@ -1,12 +1,12 @@
 import axiosInstance from "../axios-instance";
 
 // Registers a new user by sending their details to the `/register` endpoint
-export const Signup = async (data) => {
+export const signup = async (data) => {
   return await axiosInstance.post(`/register`, data);
 };
 
 // Authenticates an existing user by sending their credentials to the `/login` endpoint
-export const Login = async (data) => {
+export const login = async (data) => {
   return await axiosInstance.post(`/login`, data);
 };
 
